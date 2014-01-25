2e19c5811f3a95bcc9ca6c950db360204a892e0a
Ken Morishita <k_morishita@yumemi.co.jp>
2014-01-25 19:26:15 +0900

    init

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+GoodParts2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoodParts2::Application.config.secret_key_base = '4c4d4966387c09a397046ed2fba0c1b2bbd8002222f532edc4c3a7f77d0f3c9de82ab5a83d0653d247083bece87c0b638918bec771b4886503a1dd20f8828c7c'
